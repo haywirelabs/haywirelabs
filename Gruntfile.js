096d4e2a24780554b3c219db88c1be93baaffb86
Joe Osborne <josborne@haywirelabs.com>
2014-05-05 11:05:13 -0400

    init assemble

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,23 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    assemble: {
+      options: {
+        assets: 'assets',
+        data: ['config/config.json']
+      },
+      project: {
+        options: {
+          layout: 'layouts/default-layout.hbs',
+          partials: 'includes/**/*.hbs'
+        },
+        src: ['content/*.hbs'],
+        dest: 'build'
+      }
+    }
+  });
+
   grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-newer');
+
+  grunt.registerTask('default', ['assemble']);
 };
